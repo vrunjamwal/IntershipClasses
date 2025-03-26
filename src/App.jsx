962ec7374d1d0ad70dc484c91d8823a26153e8cb
Varun <varunjamwal933@gmail.com>
2025-03-26 13:06:50 +0530

    NEW PRO

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap (optional)
 
 
-
-import React from 'react'
-
 const App = () => {
   return (
     <div>App</div>
